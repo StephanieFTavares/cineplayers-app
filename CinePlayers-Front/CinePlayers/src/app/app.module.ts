03938695dcfd7fd50eeeb8701b2a2b5671dcdcd6
StephanieFTavares <stephaniefortava@gmail.com>
2024-05-18 23:45:30 -0300

    FEAT: Iniciado desenvolvimento

---
+++ CinePlayers-Front/CinePlayers/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
+import '@cds/core/icon/register.js';
+import { ClarityIcons, userIcon, envelopeIcon } from '@cds/core/icon';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './features/index/index.component';
+import { SalaCinemaComponent } from './features/sala-cinema/sala-cinema.component';
+import { FormUsuarioComponent } from './shared/components/form-usuario/form-usuario.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'index', component: IndexComponent },
+  { path: 'sala-cinema', component: SalaCinemaComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    SalaCinemaComponent,
+    FormUsuarioComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
+    NgxMaskModule.forRoot(),
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor() {
+    ClarityIcons.addIcons(userIcon, envelopeIcon);
+  }
+ }
